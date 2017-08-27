1c0719ab451a58369c32bcec68a85704d0b0a756
伊藤拓海 <itoutakumi@itoutakumis-MacBook-Air.local>
2017-08-27 22:19:38 +0900

    app.rb の開発の途中

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OnlineJudge_WSP
+
+## Dockerを使ったプログラム実行環境
+実践入門書のapp.jsをapp.rbにし、sinatraで実装
--- app.rb
+++ app.rb
@@ -1,12 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require_relative 'lib/excontainer'
 
 get '/' do
   slim :index
 end
 
 get '/exec' do
-  r = ExecCountainer.new(Time.now.to_f, 'c')
+  r = ExCountainer.new(Time.now.to_f, 'c')
   r.exec.to_s
 end
 
+post '/api/run' do
+  lang = params[:language]
+  sorce = params[:sorce_code]
+  input = params[:input]
+  time = Time.now.to_f
+  pfname = "#{time}.#{lang.downcase}"
+  ifname = "#{time}.in"
+
+  File.open("tmp/#{pfname}", "w") do |fl|
+    prog.split('\n').each do |i|
+      f.puts(l)
+    end
+  end
+
+  c = ExContainer.new(time, lang)
+
+  r = c.exec()
+
+  return_prm = {stdout: r[0].join(''), stderr: r[1].join(''), exit_code: r[2]}
+  content_type :json
+  retuen_prm.to_json
+end
